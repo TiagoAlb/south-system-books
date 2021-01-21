8a4f88075238829897bc72d15ba18489f98080db
Tiago Albuquerque <tiago.mauricio.albuquerque@gmail.com>
2021-01-21 00:26:08 -0300

    Inclusão de key no link de download do livro

--- src/components/BookInformation/index.js
+++ src/components/BookInformation/index.js
@@ -17,9 +17,8 @@ const BookInformation = ({ informations, mobile, actions }) => {
                 if (link) {
                     if (prop.name === 'download') {
                         return (
-                            <a href={link} download={informations.volumeInfo.title + `-${informations.id}`} target='_blank'>
+                            <a href={link} key={key} download={informations.volumeInfo.title + `-${informations.id}`} target='_blank'>
                                 <IconBtn
-                                    key={key}
                                     icon={prop.icon}
                                     title={prop.title}
                                     click={() => (
